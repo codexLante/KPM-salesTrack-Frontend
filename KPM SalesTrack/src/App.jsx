c3928858fb97281b8727fb32a4e52fe53c051921
CodexLante <allomwas@gmail.com>
2025-10-22 15:45:26 +0300

    Added a meeting schedule card, a performance chart component and worked on the dashboard

--- KPM SalesTrack/src/App.jsx
+++ KPM SalesTrack/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./ADMIN/pages/login";
 import SignUp from "./ADMIN/pages/SignUp";
 import AdminLayout from "./ADMIN/index";
+import Dashboard from "./ADMIN/pages/dashboard"; 
 import EmployeeManagement from "./ADMIN/pages/EmployeeManagement";
 
 function App() {
@@ -14,7 +15,11 @@ function App() {
         
         {/* Protected Admin Routes - Nested */}
         <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<EmployeeManagement />} />
+          {/* Dashboard as the default/index route */}
+          <Route index element={<Dashboard />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          
+          {/* Other admin pages */}
           <Route path="employees" element={<EmployeeManagement />} />
           {/* Add more routes as you create pages */}
         </Route>
